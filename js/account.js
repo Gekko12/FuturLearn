9ef241d9efb2d9ce4db2306f126d1cf81e66e41b
Gekko12 <gauravsharma01042000@gmail.com>
2021-07-11 20:29:02 +0530

    account page done

---
+++ js/account.js
@@ -0,0 +1,15 @@
+$("#profileImage").click(function(e) {
+  $("#imageUpload").click();
+});
+
+function fasterPreview( uploader ) {
+  if ( uploader.files && uploader.files[0] ){
+        $('#profileImage').attr('src', 
+           window.URL.createObjectURL(uploader.files[0]) );
+  }
+}
+
+$("#imageUpload").change(function(){
+  fasterPreview( this );
+});
+
