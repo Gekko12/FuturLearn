0b926498b4cc220ad3063f8cec071851d285649d
Gekko12 <gauravsharma01042000@gmail.com>
2021-08-15 13:35:18 +0530

    to-do page edited

--- js/account.js
+++ js/account.js
@@ -1,45 +1,31 @@
-$("#profileImage").click(function(e) {
+$("#profileImage").click(function (e) {
   $("#imageUpload").click();
 });
 
-function fasterPreview( uploader ) {
-  if ( uploader.files && uploader.files[0] ){
-        $('#profileImage').attr('src', 
-           window.URL.createObjectURL(uploader.files[0]) );
+function fasterPreview(uploader) {
+  if (uploader.files && uploader.files[0]) {
+    $('#profileImage').attr('src',
+      window.URL.createObjectURL(uploader.files[0]));
   }
 }
 
-$("#imageUpload").change(function(){
-  fasterPreview( this );
+$("#imageUpload").change(function () {
+  fasterPreview(this);
 });
 
 function add_item() {
-  
-  // Getting box and ul by selecting id;
   let item = document.getElementById("box");
   let list_item = document.getElementById("list_item");
-  if(item.value != ""){
-  
-      // Creating element and adding value to it
-      let make_li = document.createElement("LI");
-      make_li.appendChild(document.createTextNode(item.value));
-  
-      // Adding li to ul
-      list_item.appendChild(make_li);
-  
-      // Reset the value of box
-      item.value=""
-        
-      // Delete a li item on click 
-      make_li.onclick = function(){
-        this.parentNode.removeChild(this);
-      }
-  
+  if (item.value != "") {
+    let make_li = document.createElement("LI");
+    make_li.appendChild(document.createTextNode(item.value));
+    list_item.appendChild(make_li);
+    item.value = ""
+    make_li.onclick = function () {
+      this.parentNode.removeChild(this);
+    }
   }
-  else{
-  
-    // Alert msg when value of box is "" empty.
+  else {
     alert("plz add a value to item");
   }
-  
 }
